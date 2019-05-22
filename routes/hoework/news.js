4a84138aa4e205baa1159a9f156ed52ca4859ddf
yeoneei <soso3786@gmail.com>
2019-05-22 17:51:16 +0900

    [update] getting all posts odered by time

--- routes/hoework/news.js
+++ routes/hoework/news.js
@@ -10,7 +10,23 @@ const moment = require('moment');
 /* GET home page. */
 
 router.get('/',async(req,res)=>{
-
+    let getAllRegisterQuery = 'SELECT * FROM sopt.register';
+    let getAllRegisterResult;
+    try{
+        var connection = await pool.getConnection();
+        getAllRegisterResult = await connection.query(getAllRegisterQuery);
+    }catch(err){
+        console.log(err);
+        connection.rollback(()=>{});
+        res.status(200).send(util.successFalse(statusCode.DB_ERROR,resMessage.POST_READ_FAILE));
+    }finally{
+        let resData = new Array();
+        for(let i= getAllRegisterResult.length-1; i>=0;i--){
+            resData.push(getAllRegisterResult[i]);
+        }
+        pool.releaseConnection(connection);
+        res.status(200).send(util.successTrue(statusCode.OK,resMessage.POST_READ_SUCCESS,resData));
+    }
 });
 
 router.get('/:idx',async(req,res)=>{
@@ -20,14 +36,14 @@ router.get('/:idx',async(req,res)=>{
         var connection = await pool.getConnection();
         await connection.beginTransaction();
 
-        let getRegisterQuery = 'select * from sopt.register WHERE idx= ?';
+        let getRegisterQuery = 'SELECT * FROM sopt.register WHERE idx= ?';
         let getRegisterResult = await connection.query(getRegisterQuery,[idx]);
         let time = getRegisterResult[0].time;
         let title = getRegisterResult[0].title;
         console.log(time)
         await connection.commit();
 
-        let getPostQeury = 'select * from sopt.post WHERE postIdx=?';
+        let getPostQeury = 'SELECT * FROM sopt.post WHERE postIdx=?';
         getRegisterResult = await connection.query(getPostQeury,[idx]);
         console.log(getRegisterResult);
         await connection.commit();
