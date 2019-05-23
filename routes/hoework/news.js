558c4b788815152e3e09e048893398162f34ac43
yeoneei <soso3786@gmail.com>
2019-05-23 21:20:44 +0900

    [update] server Ip

--- routes/hoework/news.js
+++ routes/hoework/news.js
@@ -10,7 +10,7 @@ const moment = require('moment');
 /* GET home page. */
 
 router.get('/',async(req,res)=>{
-    let getAllRegisterQuery = 'SELECT * FROM sopt.register';
+    let getAllRegisterQuery = 'SELECT * FROM 4th.register';
     let getAllRegisterResult;
     try{
         var connection = await pool.getConnection();
@@ -36,14 +36,14 @@ router.get('/:idx',async(req,res)=>{
         var connection = await pool.getConnection();
         await connection.beginTransaction();
 
-        let getRegisterQuery = 'SELECT * FROM sopt.register WHERE idx= ?';
+        let getRegisterQuery = 'SELECT * FROM 4th.register WHERE idx= ?';
         let getRegisterResult = await connection.query(getRegisterQuery,[idx]);
         let time = getRegisterResult[0].time;
         let title = getRegisterResult[0].title;
         console.log(time)
         await connection.commit();
 
-        let getPostQeury = 'SELECT * FROM sopt.post WHERE postIdx=?';
+        let getPostQeury = 'SELECT * FROM 4th.post WHERE postIdx=?';
         getRegisterResult = await connection.query(getPostQeury,[idx]);
         console.log(getRegisterResult);
         await connection.commit();
@@ -70,7 +70,7 @@ router.post('/', upload.fields([{name:'thumb'},{name:'imgs'}]) , async(req,res)=
 
     let time = moment().format('YYYY-MM-DD HH:mm:ss');
 
-    let insertRegisterQuery = 'INSERT INTO sopt.register (title,name,thumbnail,time) VALUE (?,?,?,?)';
+    let insertRegisterQuery = 'INSERT INTO 4th.register (title,name,thumbnail,time) VALUE (?,?,?,?)';
     let insertRegisterResult;
     
     try{
@@ -81,7 +81,7 @@ router.post('/', upload.fields([{name:'thumb'},{name:'imgs'}]) , async(req,res)=
         await connection.commit();
         
         const postIdx = insertRegisterResult.insertId;
-        let insertPostQuery = 'INSERT INTO sopt.post (postIdx, photo,content) VALUE (?,?,?)';
+        let insertPostQuery = 'INSERT INTO 4th.post (postIdx, photo,content) VALUE (?,?,?)';
         let insertPostResult;
         for(idx in imgs){
             insertPostResult = await connection.query(insertPostQuery,[postIdx, imgs[idx].location,contents[idx]]);
